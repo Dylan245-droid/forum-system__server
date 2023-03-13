ccf6919e23bc12e2706484ad4ab0f2532abd22c1
Dylan245-droid <danielebang762@gmail.com>
2023-03-13 16:19:17 +0100

    13/03/2023

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.json())
 app.use(cors())
 
 const users = []
+const threadList = []
+
 const generateId = () => Math.random().toString(36).substring(2, 10)
 
 app.get("/api", (req, res) => {
@@ -40,6 +42,53 @@ app.post("/api/register", async (req, res) => {
     console.log({ email, password, username, id });
 })
 
+app.post("/api/login", async (req, res) => {
+    const { email, password } = req.body
+
+    let result = users.filter(
+        (user) => user.email === email && user.password === password
+    )
+
+    if (result.length !== 1) {
+        return res.json({
+            error_message: "Incorrect Credentials"
+        })
+    }
+
+    res.json({
+        message: "Login Successfully",
+        id: result[0].id
+    })
+})
+
+app.get("/api/all/thread", async (req, res) => {
+    res.json({
+        threads: threadList
+    })
+})
+
+app.post("/api/create/thread", async (req, res) => {
+    const { thread, userId } = req.body
+    const threadId = generateId()
+
+    threadList.unshift({
+        id: threadId,
+        title: thread,
+        userId,
+        replies: [],
+        likes: []
+    })
+
+    res.json({
+        message: "Thread created successfully!",
+        threads: threadList
+    })
+
+    console.log({ thread, userId, threadId });
+})
+
+
 app.listen(port, () => {
+    console.log(users);
     console.log(`Server listening on port ${port}`);
 })
